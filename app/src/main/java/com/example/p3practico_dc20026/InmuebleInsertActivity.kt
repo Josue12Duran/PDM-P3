e0484f63491377861e71c9f063e2a4c299ac205f
0JD0 <eusojdivad0@gmail.com>
2024-06-12 01:21:12 -0600

    View full

--- app/src/main/java/com/example/p3practico_dc20026/InmuebleInsertActivity.kt
+++ app/src/main/java/com/example/p3practico_dc20026/InmuebleInsertActivity.kt
@@ -1,46 +1,29 @@
 package com.example.p3practico_dc20026
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class InmuebleInsertActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_inmueble_insert)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val toolbar : Toolbar = findViewById(R.id.toolbarMain)
+        val toolbar: Toolbar = findViewById(R.id.toolbarInsertInmueble)
         setSupportActionBar(toolbar)
-        supportActionBar?.title = "Casa Top"
-
-        val btnVerPropietarios : Button = findViewById(R.id.btnVerPropietarios)
-        btnVerPropietarios.setOnClickListener {
-            startActivity(Intent(this, PropietarioViewActivity::class.java))
-        }
-        val btnAgregarPropietarios : Button = findViewById(R.id.btnAgregarPropietarios)
-        btnAgregarPropietarios.setOnClickListener {
-            startActivity(Intent(this, PropietarioInsertActivity::class.java))
-        }
-        val btnVerInmuebles : Button = findViewById(R.id.btnVerInmuebles)
-        btnVerInmuebles.setOnClickListener {
-            startActivity(Intent(this, InmuebleViewActivity::class.java))
-        }
-        val btnAgregarInmuebles : Button = findViewById(R.id.btnAgregarInmuebles)
-        btnAgregarInmuebles.setOnClickListener {
-            startActivity(Intent(this, InmuebleInsertActivity::class.java))
-        }
+        supportActionBar?.title = "Insertar Inmueble"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
     }
 
     override fun onCreateOptionsMenu(menu: android.view.Menu): Boolean {
--- app/src/main/java/com/example/p3practico_dc20026/InmuebleViewActivity.kt
+++ app/src/main/java/com/example/p3practico_dc20026/InmuebleViewActivity.kt
@@ -1,46 +1,29 @@
 package com.example.p3practico_dc20026
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class InmuebleViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_inmueble_view)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val toolbar : Toolbar = findViewById(R.id.toolbarMain)
+        val toolbar: Toolbar = findViewById(R.id.toolbarViewInmueble)
         setSupportActionBar(toolbar)
-        supportActionBar?.title = "Casa Top"
-
-        val btnVerPropietarios : Button = findViewById(R.id.btnVerPropietarios)
-        btnVerPropietarios.setOnClickListener {
-            startActivity(Intent(this, PropietarioViewActivity::class.java))
-        }
-        val btnAgregarPropietarios : Button = findViewById(R.id.btnAgregarPropietarios)
-        btnAgregarPropietarios.setOnClickListener {
-            startActivity(Intent(this, PropietarioInsertActivity::class.java))
-        }
-        val btnVerInmuebles : Button = findViewById(R.id.btnVerInmuebles)
-        btnVerInmuebles.setOnClickListener {
-            startActivity(Intent(this, InmuebleViewActivity::class.java))
-        }
-        val btnAgregarInmuebles : Button = findViewById(R.id.btnAgregarInmuebles)
-        btnAgregarInmuebles.setOnClickListener {
-            startActivity(Intent(this, InmuebleInsertActivity::class.java))
-        }
+        supportActionBar?.title = "Ver Inmuebles"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
     }
 
     override fun onCreateOptionsMenu(menu: android.view.Menu): Boolean {
